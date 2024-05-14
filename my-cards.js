49a0d30f6d96444d4f8d55fcf9815456d1f325c4
Szymon Tyburek <szymontyburek10@gmail.com>
2024-05-13 23:52:59 -0700

    added create_card function

--- my-cards.js
+++ my-cards.js
@@ -5,21 +5,6 @@ class Cards extends HTMLElement {
     return this;
   }
 
-  //     <div id="card" style="background-color: red">
-  //     <h3>Card 1</h3>
-  //     <p>
-  //       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
-  //       accusantium possimus et, quasi beatae odit. Qui, maxime. Aperiam
-  //       assumenda sapiente quis unde nobis exercitationem amet nostrum
-  //       dolorem. Corrupti, quia architecto repellendus perferendis earum, sed
-  //       praesentium, ullam quae pariatur ipsum saepe? Animi dolorem iure quis
-  //       hic. Tempore illum hic qui deleniti, necessitatibus laboriosam nobis
-  //       quia nihil et dolorum amet velit consectetur ut sapiente a optio quasi
-  //       explicabo reprehenderit harum, provident totam nemo. Deleniti quas
-  //       impedit quos quia perspiciatis dolor earum magnam?
-  //     </p>
-  //   </div>
-
   connectedCallback() {
     //add media queries to the shadowDom
     const style = document.createElement("style");
@@ -28,6 +13,23 @@ class Cards extends HTMLElement {
 
     this.init = function (config) {
       debugger;
+    };
+
+    const create_card = function () {
+      //     <div id="card" style="background-color: red">
+      //     <h3>Card 1</h3>
+      //     <p>
+      //       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam
+      //       accusantium possimus et, quasi beatae odit. Qui, maxime. Aperiam
+      //       assumenda sapiente quis unde nobis exercitationem amet nostrum
+      //       dolorem. Corrupti, quia architecto repellendus perferendis earum, sed
+      //       praesentium, ullam quae pariatur ipsum saepe? Animi dolorem iure quis
+      //       hic. Tempore illum hic qui deleniti, necessitatibus laboriosam nobis
+      //       quia nihil et dolorum amet velit consectetur ut sapiente a optio quasi
+      //       explicabo reprehenderit harum, provident totam nemo. Deleniti quas
+      //       impedit quos quia perspiciatis dolor earum magnam?
+      //     </p>
+      //   </div>
     };
   }
 }
